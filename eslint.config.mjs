8ebfca5a60388bbf5b732e4143dbff310751e9d6
palak <savanipalak27@gmail.com>
2025-06-06 15:42:11 +0530

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// Run this command to generate base config and vs code settings:
+// pnpm dlx @antfu/eslint-config@latest
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import antfu from "@antfu/eslint-config";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
+export default antfu({
+  type: "app",
+  next: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  ignores: [
+    "routeTree.gen.ts",
+    "src/assets/lottie/animation-loader.json",
+    "src/components/ui", // add ui directory files
+  ],
+}, {
+  rules: {
+    "eslint-comments/no-unlimited-disable": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "no-redeclare": "off",
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "style/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+  },
 });
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
