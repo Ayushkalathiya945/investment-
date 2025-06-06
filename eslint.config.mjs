925e652ecc7cc16637444e859a62ed8a327fd576
palak <savanipalak27@gmail.com>
2025-06-06 16:34:49 +0530

    fix:lint set up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,35 +2,49 @@
 // pnpm dlx @antfu/eslint-config@latest
 
 import antfu from "@antfu/eslint-config";
+import nextPlugin from "@next/eslint-plugin-next";
 
-export default antfu({
-  type: "app",
-  next: true,
-  typescript: true,
-  formatters: true,
-  stylistic: {
-    indent: 2,
-    semi: true,
-    quotes: "double",
+export default antfu(
+  {
+    type: "app",
+    react: true,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+    ignores: [
+      "**/dist",
+      "**/node_modules",
+      "!.storybook",
+      "storybook-static",
+      "!.next",
+    ],
   },
-  ignores: [
-    "routeTree.gen.ts",
-    "src/assets/lottie/animation-loader.json",
-    "src/components/ui", // add ui directory files
-  ],
-}, {
-  rules: {
-    "eslint-comments/no-unlimited-disable": "off",
-    "unused-imports/no-unused-imports": "warn",
-    "no-redeclare": "off",
-    "ts/consistent-type-definitions": ["error", "type"],
-    "no-console": ["warn"],
-    "antfu/no-top-level-await": ["off"],
-    "node/prefer-global/process": ["off"],
-    "node/no-process-env": ["error"],
-    "perfectionist/sort-imports": ["error", {
-      tsconfigRootDir: ".",
-    }],
-    "style/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
   },
-});
+  {
+    rules: {
+      "eslint-comments/no-unlimited-disable": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "no-redeclare": "off",
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+      "perfectionist/sort-imports": ["error", {
+        tsconfigRootDir: ".",
+      }],
+      "style/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+    },
+  });
